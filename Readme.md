766c2a6e11f545fe2dfabad0886b473d4a982a17
汐雪 <akb011335@gmail.com>
2024-08-22 16:48:43 +0800

    0822v1.0.0

--- Readme.md
+++ Readme.md
@@ -1,3 +1,30 @@
-# PTT 爬蟲程式 v0.0.1
+# PTT 爬蟲程式 v1.0.0
+---
 
 
+## 建立環境
+```bash
+pip install -r requirements.txt
+```
+
+
+## 將 Line Notify Token 添加到 .env
+```bash
+Token="Your Line Notify Token"
+```
+你可以在 [Line Offical Websit](https://notify-bot.line.me/my/)申請 Token.
+
+
+## 執行腳本以取得爬蟲結果
+```bash
+python main.py <target-board>
+```
+範例：
+```bash
+python main.py marvel
+```
+結果將儲存在專案的根目錄。
+
+
+## 聯絡資訊
+如果你有任何問題或疑慮，請使用 GitHub Issue 提出討論。
--- main.py
+++ main.py
@@ -1,32 +1,25 @@
 import time
 import requests
 import pandas
+import datetime
+import json
+import sys
+import os
 from bs4 import BeautifulSoup
-import scipy
-
-
+from dotenv import load_dotenv
+load_dotenv()
+# 常數
 # 目標網址
-target_url = "https://www.ptt.cc/bbs/"
+TARGET_URL = "https://www.ptt.cc/bbs/"
+# 目標頁面
+TARGET_PAGE = "/index"
+# 頁面附屬檔名
+PAGE_EXT = ".html"
+# Requests 請求標頭
 
-# 目標看板
-target_board = "marvel"
-
-#目標頁面
-target_page = "/index"
-
-#目標頁數
-page_num = ""
-
-#頁面附屬檔名
-page_ext = ".html"
-
-target = target_url + target_board + target_page + page_num  + page_ext
-
-headers = {
+HEADERS = {
     "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
 }
-
-
 
 def download_html(target, headers):
     return requests.get(target, headers= headers)
@@ -50,7 +43,7 @@ def parser_article_content(url_list):
     for url_ in url_list:
         if url_ != None:
             article_url = 'https://www.ptt.cc' + url_
-            page_data = download_html(article_url, headers)
+            page_data = download_html(article_url, HEADERS)
             page_html_code = BeautifulSoup(page_data.content, features="html.parser")
             try:
                 article_data = page_html_code.find_all('span', class_="article-meta-value")
@@ -76,11 +69,62 @@ def export_json(data):
     ptt_df.to_json("ptt.json")
     return True
 
+def main():
+    print("接收參數的長度:", len(sys.argv))
+    if len(sys.argv) < 2:
+        print("缺少參數: 爬蟲的目標看板!")
+        sys.exit()
+    else:
+        if len(sys.argv) == 2:
+            page_num = ""
+            print("為偵測到目標頁數，因此只進行最新文章頁面進行爬蟲")
+        else:
+            page_num = sys.argv[2]
+                   
+        target_board = sys.argv[1]
+        
+        target = TARGET_URL + target_board + TARGET_PAGE + page_num  + PAGE_EXT
+        print(target)
+        '''爬蟲執行順序
+             download_html() -> parser_board_urls() -> parser_article_content() -> export_json()
+             
+             download_html(): arg1: 網址, arg2: 請求標頭, returns: requests class
+             parser_board_urls: arg1: request class, return: (list)urls
+             parser_article_content: arg1: (list) urls, return: (list) ppt_data
+             export_json: arg1: (list) ppt_data, return: (Boolean) True, Output: ptt.json
+            
+        '''
+        # 爬蟲主程式
+        res = download_html(target, HEADERS)
+        url_list = parser_board_urls(res)
+        ptt_data = parser_article_content(url_list)
+        export_json(ptt_data)
+        
+        #發動訊息
+        time_str = "{date:%Y-%m-%d %H:%M:%S}".format(date=datetime.datetime.now())
+        process_st = send_line_notify("{}- 看板-{} 爬蟲已完成".format("", target_board))
+        if process_st == 200:
+            print("Line Notify 通知完成")
+        else:
+            print("Line Notify API Error")
+        
+        
+    return None
 
+def send_line_notify(msg = "傳輸訊息"):
+    line_notify_url = "https://notify-api.line.me/api/notify"
+    line_notify_token = os.getenv("Token")
+    line_notify_header = {
+        'Authorization': 'Bearer {}'.format(line_notify_token)
+        }
+    line_notify_body = {
+        'message':  msg
+        }
+    
+    res = requests.post(line_notify_url, headers=line_notify_header, data = line_notify_body)
+    res_msg = json.loads(res.text)
+    return res_msg['status']
 
-## Test
-a = download_html(target, headers)
-b = parser_board_urls(a)
-c = parser_article_content(b)
-export_json(c)
-print(c)
+#加入__main__ 執行區段
+if __name__ == '__main__':
+    main()
